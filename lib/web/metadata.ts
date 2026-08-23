b87d44dd7a39c978f6734b743903831532a8e2ef
JordanAlec <57456980+JordanAlec@users.noreply.github.com>
2026-08-23 23:44:27 +0100

    chore(branding): capitalize SoundPrint in package name and description

--- lib/web/metadata.ts
+++ lib/web/metadata.ts
@@ -2,5 +2,5 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "SoundPrint",
-  description: "Your personal soundprint, a unique fingerprint of your auditory profile.",
+  description: "Your personal 'SoundPrint', a unique fingerprint of your auditory profile.",
 };
